bb0bcf91eaeb258ea719f8366e0a1f19f9c61233
Palak Arora <your.email@example.com>
2026-01-16 22:53:42 +0530

    Initial commit: Greeting library and console app

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using GreetingLibrary;
+
+Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+
+string greeting = GreetingHelper.DisplayGreeting(name);
+Console.WriteLine(greeting);
+
---
+++ GreetingLibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string DisplayGreeting(string name)
+        {
+            return $"Hello,{name}!";
+        }
+    }
+}
+
