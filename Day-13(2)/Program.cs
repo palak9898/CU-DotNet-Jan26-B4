07bdc603eec13b62512158fe5e6bab584f7c0efc
Palak Arora <your.email@example.com>
2026-01-21 14:58:10 +0530

    display line

---
+++ Day-13(2)/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Day13
+{
+    public static void Main(string[] args){
+        displayOutput();
+        displayOutput(ch: '$');
+        displayOutput(60, '+');
+        displayOutput(70);
+    }
+    static void displayOutput(int ch = 40, char chr = '-'){
+
+        for(int i=0;i< ch; i++)
+        {
+            Console.Write(chr);
+            
+        }
+        Console.WriteLine(" ");
+            
+    }
+    
+}
