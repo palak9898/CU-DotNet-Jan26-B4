fb0b8b8a8d0ddf5ff63144623c857f129ebff71d
Palak Arora <palakarora9898@gmail.com>
2026-02-03 15:08:38 +0530

    HashTable

---
+++ Day-24(3)/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class SortedDictPractice
+{
+    static void Main(string[] args)
+    {
+        SortedDictionary<double,string> dict = new SortedDictionary<double, string>();
+        dict.Add(55.42,"SwiftRacer");
+        dict.Add(52.10,"SpeedDemon");
+        dict.Add(58.91,"SteadyEddie");
+        dict.Add(51.05,"TurboTom");
+
+        foreach(var item in dict)
+        {
+            System.Console.WriteLine(item.Key + " " + item.Value);
+        }
+
+        System.Console.WriteLine("First item is: "+ dict.Keys.First()+ " Last item is: " + dict.Keys.Last()); 
+        double idToRemove=0;
+        foreach(var item in dict)
+        {
+            
+            if(item.Value == "SteadyEddie")
+            {
+                idToRemove = item.Key;     
+            }
+        }
+        if(idToRemove!=0){
+        dict.Remove(idToRemove);
+        }
+        
+        System.Console.WriteLine("Deletion done");
+        dict.Add(54.00,"SteadyEddie");
+        
+
+        Console.WriteLine("List after updation ");
+        System.Console.WriteLine();
+        foreach(var item in dict)
+        {
+            System.Console.WriteLine( item.Key + " " + item.Value);
+        }
+    }
+}
