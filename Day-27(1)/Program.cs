c67dd2d47837c9b0c631d6b8776ac4c5dfbbd817
Palak Arora <palakarora9898@gmail.com>
2026-02-06 15:15:58 +0530

    File handling basics

---
+++ Day-27(1)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Day27
+{
+    static void Main(string[] args)
+    {
+        
+        
+        string file = @"journal.txt";
+        
+        try
+        {
+            //FileStream fs = new FileStream(File, FileMode.Append);
+            using StreamWriter sw  = new StreamWriter(file,true);
+            Console.WriteLine("Daily Reflection Entry");
+            do
+            {
+            string input = Console.ReadLine();
+            if(input == "stop")
+            {
+                break;
+            }
+            sw.WriteLine(input);
+            }
+            while(true);
+        }
+        catch(FileNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch
+        {
+            System.Console.WriteLine("Error while opening file");
+        }
+        
+    }
+}
